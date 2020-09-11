20a7da17192009df23c11a92657fa35cef4863e4
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-11 13:02:29 +0200

    book list styles added

--- src/app/components/layout/book-list/book-list.component.ts
+++ src/app/components/layout/book-list/book-list.component.ts
@@ -9,12 +9,32 @@ import { Book } from '../../../models';
 })
 export class BookListComponent implements OnInit {
   books: Book[];
+  readPages: number;
+  pagesToRead: number;
+  showReadPages: boolean = true;
+  showPagesToRead: boolean = true;
 
   constructor(private bookService: BookService) {}
 
   ngOnInit(): void {
     this.bookService.getBooks().subscribe((data) => {
       this.books = data;
+      this.getPagesToRead();
+      this.getReadPages();
     });
+  }
+
+  getPagesToRead(): void {
+    const notReadBooks = this.books.filter((book) => !book.isRead);
+    this.pagesToRead = notReadBooks.reduce((total, book) => {
+      return (total += book.pages);
+    }, 0);
+  }
+
+  getReadPages(): void {
+    const readBooks = this.books.filter((book) => book.isRead);
+    this.readPages = readBooks.reduce((total, book) => {
+      return (total += book.pages);
+    }, 0);
   }
 }
