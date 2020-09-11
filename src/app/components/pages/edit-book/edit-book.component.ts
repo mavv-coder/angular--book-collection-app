b5182fad2a571468f00f87333b2f03b7273b9b84
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-11 13:38:58 +0200

    book saved by params id

--- src/app/components/pages/edit-book/edit-book.component.ts
+++ src/app/components/pages/edit-book/edit-book.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router, ActivatedRoute } from '@angular/router';
+import { BookService } from '../../../services/book/book.service';
+import { Book } from 'src/app/models';
 
 @Component({
   selector: 'app-edit-book',
   templateUrl: './edit-book.component.html',
-  styleUrls: ['./edit-book.component.scss']
+  styleUrls: ['./edit-book.component.scss'],
 })
 export class EditBookComponent implements OnInit {
+  id: string;
+  book: Book;
 
-  constructor() { }
+  constructor(
+    private bookService: BookService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private flashMessage: FlashMessagesService
+  ) {}
 
   ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.bookService.getBook(this.id).subscribe((data) => {
+      this.book = data;
+    });
   }
-
 }
