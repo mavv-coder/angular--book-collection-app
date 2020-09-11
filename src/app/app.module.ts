f59bf7964a004017fb8ee1fd9ad83ea64b82ed6e
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-11 09:36:27 +0200

    firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+// angular2-flash-messages
 import { FlashMessagesModule } from 'angular2-flash-messages';
 
+// firebase
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+// components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
@@ -26,7 +35,15 @@ import { NotFoundComponent } from './components/pages/not-found/not-found.compon
     LoginComponent,
     NotFoundComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FlashMessagesModule.forRoot()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule,
+    FlashMessagesModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
