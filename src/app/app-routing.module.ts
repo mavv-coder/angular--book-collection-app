e2a7c5ed90dd2bd2396a7196714fbd575ab1dc07
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-11 09:05:06 +0200

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { AddBookComponent } from './components/pages/add-book/add-book.component';
+import { BookDetailComponent } from './components/pages/book-detail/book-detail.component';
+import { EditBookComponent } from './components/pages/edit-book/edit-book.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'book/:id', component: BookDetailComponent },
+  { path: 'book/edit/:id', component: EditBookComponent },
+  { path: 'book/add', component: AddBookComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
 import { BookListComponent } from './components/layout/book-list/book-list.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { AddBookComponent } from './components/pages/add-book/add-book.component';
+import { BookDetailComponent } from './components/pages/book-detail/book-detail.component';
+import { EditBookComponent } from './components/pages/edit-book/edit-book.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, BookListComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BookListComponent,
+    DashboardComponent,
+    AddBookComponent,
+    BookDetailComponent,
+    EditBookComponent,
+    LoginComponent,
+    NotFoundComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
