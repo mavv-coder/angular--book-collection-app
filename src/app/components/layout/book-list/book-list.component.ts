b6f82a2f6db75628bf9b8ef84ff183c9ef119fcf
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-09-12 18:12:00 +0200

    add book btn added

--- src/app/components/layout/book-list/book-list.component.ts
+++ src/app/components/layout/book-list/book-list.component.ts
@@ -10,10 +10,6 @@ import { Book } from '../../../models';
 })
 export class BookListComponent implements OnInit {
   books: Book[];
-  readPages: number;
-  pagesToRead: number;
-  showReadPages: boolean = true;
-  showPagesToRead: boolean = true;
 
   constructor(
     private bookService: BookService,
@@ -23,23 +19,7 @@ export class BookListComponent implements OnInit {
   ngOnInit(): void {
     this.bookService.getBooks().subscribe((data) => {
       this.books = data;
-      this.getPagesToRead();
-      this.getReadPages();
     });
-  }
-
-  getPagesToRead(): void {
-    const notReadBooks = this.books.filter((book) => !book.isRead);
-    this.pagesToRead = notReadBooks.reduce((total, book) => {
-      return (total += book.pages);
-    }, 0);
-  }
-
-  getReadPages(): void {
-    const readBooks = this.books.filter((book) => book.isRead);
-    this.readPages = readBooks.reduce((total, book) => {
-      return (total += book.pages);
-    }, 0);
   }
 
   onDelete(book: Book): void {
--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../../services/book/book.service';
+import { Book } from '../../../models';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  books: Book[];
+  readPages: number;
+  pagesToRead: number;
+  showReadPages: boolean = true;
+  showPagesToRead: boolean = true;
+  constructor(private bookService: BookService) {}
 
   ngOnInit(): void {
+    this.bookService.getBooks().subscribe((data) => {
+      this.books = data;
+      this.getPagesToRead();
+      this.getReadPages();
+    });
   }
 
+  getPagesToRead(): void {
+    const notReadBooks = this.books.filter((book) => !book.isRead);
+    this.pagesToRead = notReadBooks.reduce((total, book) => {
+      return (total += book.pages);
+    }, 0);
+  }
+
+  getReadPages(): void {
+    const readBooks = this.books.filter((book) => book.isRead);
+    this.readPages = readBooks.reduce((total, book) => {
+      return (total += book.pages);
+    }, 0);
+  }
 }
